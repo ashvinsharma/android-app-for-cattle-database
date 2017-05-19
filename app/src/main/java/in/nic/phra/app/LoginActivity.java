5d92c61901fb3a80b32b103e68302a5d2524de5c
ashvinsharma <ashvinsharma97@gmail.com>
2017-05-19 11:11:38 +0530

    meH

--- app/src/main/java/in/nic/phra/app/LoginActivity.java
+++ app/src/main/java/in/nic/phra/app/LoginActivity.java
@@ -2,12 +2,14 @@ package in.nic.phra.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginButton;
     EditText editTextUsername, editTextPassword;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,9 @@ public class LoginActivity extends AppCompatActivity {
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
     }
 
-    public void sen
+    public void sendLoginRequest(View view){
+        username = editTextUsername.getText().toString();
+        password = editTextPassword.getText().toString();
+        //connect to DB and send the login request through a webservice SOAP/REST
+    }
 }
